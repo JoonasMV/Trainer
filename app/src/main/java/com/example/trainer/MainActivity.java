570b9ffc3405c791ecdfa7ffe8f230572cc6ae46
Joonas V <JoonasMRViljanen@gmail.com>
2023-01-15 10:00:22 +0200

    Button functionality added

--- app/src/main/java/com/example/trainer/MainActivity.java
+++ app/src/main/java/com/example/trainer/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.trainer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button exercisesBtn;
+    Button workoutsBtn;
+    Button progressBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        exercisesBtn = findViewById(R.id.exercisesBtn);
+        workoutsBtn = findViewById(R.id.workoutsBtn);
+        progressBtn = findViewById(R.id.progressBtn);
+
+        exercisesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
+
+        workoutsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
+
+        progressBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+            }
+        });
     }
 }
