22fb484bc46ee1eda3421a35a18a74af11f707d6
Joonas V <JoonasMRViljanen@gmail.com>
2023-01-15 10:40:41 +0200

    Login page added

--- app/src/main/java/com/example/trainer/MainActivity.java
+++ app/src/main/java/com/example/trainer/MainActivity.java
@@ -6,17 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button exercisesBtn;
     Button workoutsBtn;
     Button progressBtn;
-
+    TextView userGreetText;
+    String username = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = getIntent().getStringExtra("username");
+        userGreetText = findViewById(R.id.userGreetText);
+        userGreetText.setText("Welcome back " + username);
+
+        if (username == null || username.length() == 0) {
+            startActivity(new Intent(MainActivity.this, LoginPage.class));
+        }
+
         exercisesBtn = findViewById(R.id.exercisesBtn);
         workoutsBtn = findViewById(R.id.workoutsBtn);
         progressBtn = findViewById(R.id.progressBtn);
@@ -41,5 +52,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, SecondActivity.class));
             }
         });
+
+
     }
 }
