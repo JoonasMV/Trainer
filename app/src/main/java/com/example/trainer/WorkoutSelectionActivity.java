9b9830a488d224b1699422719b5fe531997db2cc
Joonas V <JoonasMRViljanen@gmail.com>
2023-01-21 07:36:41 +0200

    RecyclerView created

--- app/src/main/java/com/example/trainer/WorkoutSelectionActivity.java
+++ app/src/main/java/com/example/trainer/WorkoutSelectionActivity.java
@@ -1,10 +1,16 @@
 package com.example.trainer;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -15,10 +21,18 @@ public class WorkoutSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.workout_selection);
-        ArrayList<String> workouts = new ArrayList<String>();
 
-        ListView workoutList = findViewById(R.id.workoutList);
-        workoutList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1));
+        ArrayList<String> workouts = new ArrayList<>();
+        workouts.add("test1");
+        workouts.add("test2");
+        workouts.add("test3");
+
+        RecyclerView workoutList = findViewById(R.id.workoutList);
+        WorkoutListAdapter adapter = new WorkoutListAdapter(this, workouts);
+        workoutList.setAdapter(adapter);
+        workoutList.setLayoutManager(new LinearLayoutManager(this));
+
+
 
     }
 }
