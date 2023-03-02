ed2e1ec8ae12bb7337d5a5e429ab64764aed3265
Jenni Harakka <jvharakka@gmail.com>
2023-03-02 19:18:47 +0200

    delete button to presets

--- app/src/main/java/com/example/trainer/workouts/PresetAdapter.java
+++ app/src/main/java/com/example/trainer/workouts/PresetAdapter.java
@@ -1,8 +1,10 @@
 package com.example.trainer.workouts;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.trainer.R;
 import com.example.trainer.database.dao.WorkoutDAO;
 import com.example.trainer.database.schemas.Workout;
+import com.example.trainer.util.Toaster;
 import com.example.trainer.workouts.currentWorkout.CurrentWorkoutFragment;
 import com.example.trainer.workouts.currentWorkout.WorkoutManager;
 import com.example.trainer.workouts.mainActivity.MainActivity;
@@ -25,6 +28,7 @@ public class PresetAdapter extends RecyclerView.Adapter<PresetAdapter.ViewHolder
     private WorkoutDAO workoutDAO;
     private FragmentManager fManager;
 
+    private Context parentContext;
 
     public PresetAdapter(List<Workout> workoutPresets, FragmentManager fManager) {
         this.fManager = fManager;
@@ -35,16 +39,19 @@ public class PresetAdapter extends RecyclerView.Adapter<PresetAdapter.ViewHolder
     public class ViewHolder extends RecyclerView.ViewHolder {
         public TextView workoutTitle;
 
+        private Button deleteButton;
         public ViewHolder(View view) {
             super(view);
 
             workoutTitle = view.findViewById(R.id.preset);
+            deleteButton = view.findViewById(R.id.deletePresetButton);
         }
     }
 
     @NonNull
     @Override
     public PresetAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        parentContext = parent.getContext();
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.preset_item, parent, false);
 
@@ -60,6 +67,13 @@ public class PresetAdapter extends RecyclerView.Adapter<PresetAdapter.ViewHolder
             WorkoutManager.getInstance().startWorkoutFromPreset(workout);
 
             fManager.beginTransaction().replace(R.id.mainContainer, new CurrentWorkoutFragment()).commit();
+        });
+
+        holder.deleteButton.setOnClickListener(view -> {
+            workoutDAO.delete(workout);
+            workoutPresets.remove(workout);
+            Toaster.toast(parentContext, "Preset removed!");
+            notifyDataSetChanged();
         });
 
     }
