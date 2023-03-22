080b2155f5eb902331e68790719f8bbaeea65b88
Joonas V <JoonasMRViljanen@gmail.com>
2023-03-22 21:05:19 +0200

    tests added

---
+++ app/src/test/java/com/example/trainer/ServerDatabaseTests.java
@@ -0,0 +1,69 @@
+package com.example.trainer;
+
+import com.example.trainer.schemas.ExerciseType;
+import com.example.trainer.schemas.User;
+import com.example.trainer.serverConnector.DevService;
+import com.example.trainer.serverConnector.Server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ServerDatabaseTests {
+    private static Server server;
+    private static DevService devServer;
+
+    @BeforeClass
+    public static void init() {
+        server = Server.getInstance();
+        devServer = new DevService();
+
+    }
+
+    @Before
+    public void beforeEach() {
+        devServer.clearDatabase();
+    }
+
+    @Test
+    public void usersCanBeAdded() {
+        User user = new User("test user");
+        User addedUser = server.user().save(user);
+        assertEquals("test user", addedUser.getUsername());
+        assertNotNull(addedUser.getId());
+    }
+
+    @Test
+    public void userCanBeFoundByID() {
+        User addedUser = addUserToDb();
+        String userId = addedUser.getId();
+        User userById = server.user().getById(userId);
+        assertNotNull(userById);
+        assertEquals(addedUser.getUsername(), userById.getUsername());
+    }
+
+    @Test
+    public void userListCanBeFound() {
+        addUserToDb();
+        addUserToDb();
+        List<User> userList = server.user().getAll();
+        assertEquals(userList.size(),2);
+    }
+
+    private User addUserToDb() {
+        User user = new User("test user");
+        return server.user().save(user);
+    }
+
+    @Test
+    public void exerciseTypesCanBeAdded() {
+        ExerciseType exerciseType = new ExerciseType("test exerciseType");
+        ExerciseType addedExercise = server.exerciseType().save(exerciseType);
+        assertEquals(addedExercise.getExerciseTypeName(), "test exerciseType");
+        assertNotNull(addedExercise.getId());
+    }
+}
