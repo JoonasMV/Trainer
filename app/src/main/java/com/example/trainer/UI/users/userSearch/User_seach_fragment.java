b43bd19f595cbb71652192b085ed92f3753b9eb0
Joonas V <JoonasMRViljanen@gmail.com>
2023-04-21 20:46:47 +0300

    user search fragment added recyclerView

--- app/src/main/java/com/example/trainer/UI/users/userSearch/User_seach_fragment.java
+++ app/src/main/java/com/example/trainer/UI/users/userSearch/User_seach_fragment.java
@@ -2,7 +2,10 @@ package com.example.trainer.UI.users.userSearch;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,19 +13,10 @@ import android.view.ViewGroup;
 
 import com.example.trainer.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link User_seach_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class User_seach_fragment extends Fragment {
 
     public User_seach_fragment() {
         // Required empty public constructor
-    }
-    public static User_seach_fragment newInstance(String param1, String param2) {
-        User_seach_fragment fragment = new User_seach_fragment();
-        return fragment;
     }
 
     @Override
@@ -40,5 +34,16 @@ public class User_seach_fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.user_seach_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
+        // Creation of username list recyclerView
+        RecyclerView listOfUsers = view.findViewById(R.id.userNameList);
+        UserSearchAdapter adapter = new UserSearchAdapter(null);
+        listOfUsers.setAdapter(adapter);
+        listOfUsers.setLayoutManager(new LinearLayoutManager(getContext()));
+
     }
 }
