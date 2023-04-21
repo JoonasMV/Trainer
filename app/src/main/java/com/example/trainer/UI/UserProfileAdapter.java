f143fb251bd40c35b4d0ac4f2bf6692926bb73e1
Jenni Harakka <jvharakka@gmail.com>
2023-04-21 19:52:42 +0300

    new popup menu and fragment for users workout stats

--- app/src/main/java/com/example/trainer/UI/UserProfileAdapter.java
+++ app/src/main/java/com/example/trainer/UI/UserProfileAdapter.java
@@ -2,10 +2,12 @@ package com.example.trainer.UI;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,9 +17,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.trainer.R;
 import com.example.trainer.UI.workouts.workoutStats.WorkoutStatsExerciseAdapter;
 import com.example.trainer.UI.workouts.workoutStats.WorkoutStatsSetAdapter;
+import com.example.trainer.UI.workouts.workoutStats.WorkoutStats_fragment;
 import com.example.trainer.model.Exercise;
 import com.example.trainer.model.ExerciseSet;
 import com.example.trainer.model.Workout;
+import com.example.trainer.util.Toaster;
 
 import java.util.List;
 
@@ -28,6 +32,7 @@ public class UserProfileAdapter extends RecyclerView.Adapter<UserProfileAdapter.
     private Context parentContext;
 
 
+    private PopupMenu ppMenu;
 
     public UserProfileAdapter(List<Workout> workouts, Context parentContext) {
         this.workouts = workouts;
@@ -67,10 +72,38 @@ public class UserProfileAdapter extends RecyclerView.Adapter<UserProfileAdapter.
 
         holder.workoutTitle.setText(workout.getName());
 
+        holder.workoutTitle.setOnClickListener(view -> {
+            showWorkout(view, workout);
+        });
 
+        holder.button.setOnClickListener(view -> {
+            ppMenu = new PopupMenu(view.getContext(), holder.button );
+            ppMenu.getMenuInflater().inflate(R.menu.user_popup_menu, ppMenu.getMenu());
+            ppMenu.setOnMenuItemClickListener(menuItem -> {
+                switch (menuItem.getItemId()) {
+                    case R.id.show:
+                        showWorkout(view, workout);
+                        break;
+                    case R.id.save:
+                        //TODO: to be seen
+                        break;
+                }
+                return true;
+            });
+            ppMenu.show();
+        });
+
+
+        }
+
+
+    private void showWorkout(View view, Workout workout) {
+        Bundle args = new Bundle();
+        args.putSerializable(null, workout);
+        ((MainActivity) view.getContext()).getSupportFragmentManager().beginTransaction()
+                .replace(R.id.mainContainer, UserWorkoutStats_fragment.newInstance(workout), null)
+                .addToBackStack(null).commit();
     }
-
-
     @Override
     public int getItemCount() {
         return workouts.size();
---
+++ app/src/main/java/com/example/trainer/UI/UserWorkoutStats_fragment.java
@@ -0,0 +1,86 @@
+package com.example.trainer.UI;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.trainer.R;
+import com.example.trainer.UI.workouts.workoutStats.WorkoutStatsExerciseAdapter;
+import com.example.trainer.UI.workouts.workoutStats.WorkoutStats_fragment;
+import com.example.trainer.model.Exercise;
+import com.example.trainer.model.Workout;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class UserWorkoutStats_fragment extends Fragment {
+
+    private TextView workoutName;
+    private TextView workoutTime;
+    private TextView workoutDuration;
+    private RecyclerView exercises;
+    Workout workout;
+
+    public UserWorkoutStats_fragment() {
+        // Required empty public constructor
+    }
+
+
+    public static UserWorkoutStats_fragment newInstance(Workout workout) {
+        UserWorkoutStats_fragment fragment = new UserWorkoutStats_fragment();
+        Bundle args = new Bundle();
+        args.putSerializable(null, workout);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        if (container != null) {
+            container.removeAllViews();
+        }
+        View view = inflater.inflate(R.layout.workout_stats_fragment, container, false);
+        workoutName = view.findViewById(R.id.workoutName);
+        workoutTime = view.findViewById(R.id.workoutTime);
+        workoutDuration = view.findViewById(R.id.wDuration);
+        exercises = view.findViewById(R.id.exerciseRecyclerView);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+
+        workout = (Workout) getArguments().get(null);
+
+        Date time = workout.getWorkoutStarted();
+        workoutName.setText(workout.getName());
+        SimpleDateFormat DateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String str = DateFormat.format(time);
+        workoutTime.setText(str);
+        workoutDuration.setText(workout.getDuration());
+
+        List<Exercise> exerciseTypes = workout.getExercises();
+
+        WorkoutStatsExerciseAdapter adapter = new WorkoutStatsExerciseAdapter(exerciseTypes, getContext());
+        exercises.setLayoutManager(new LinearLayoutManager(getContext()));
+        exercises.setAdapter(adapter);
+
+    }
+}
